44a5e1fa298f8d8dfa8f186eab8b2045cbbc7189
= <danishfaisal.wwe@gmail.com>
2021-09-13 13:18:17 +0530

    Create an XHR Request

--- _scripts/app.js
+++ _scripts/app.js
@@ -1,3 +1,16 @@
 'use strict';
 
 const url = 'https://us-street.api.smartystreets.com/street-address?key=21102174564513388&street=86%20Frontage%20Road&city=Belmont&state=MA&candidates=10';
+
+const createRequest = function (url) {
+    const httpRequest = new XMLHttpRequest();
+    httpRequest.addEventListener('readystatechange', (url) => {
+        if (httpRequest.readyState === 4) {
+            console.log(httpRequest.responseText);
+        }
+    });
+    httpRequest.open('GET', url);
+    httpRequest.send();
+}
+
+createRequest(url);
